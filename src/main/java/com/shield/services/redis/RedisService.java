7cafd2335ef98510196319445a4ef9889678250e
Grizvold <ruslan105@gmail.com>
2023-07-22 19:32:07 +0300

    added logger and error handling

--- src/main/java/com/shield/services/redis/RedisService.java
+++ src/main/java/com/shield/services/redis/RedisService.java
@@ -1,17 +1,20 @@
 package com.shield.services.redis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import spark.Request;
 import spark.Response;
 
 public class RedisService {
+    private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
     private Jedis jedis;
 
     public RedisService() {
         try {
             this.jedis = new Jedis("localhost", 6379);
         } catch (Exception e) {
-            System.err.println("Error connecting to Redis: " + e.getMessage());
+            logger.error("Error connecting to Redis: " + e.getMessage());
             System.exit(1);
         }
     }
@@ -25,19 +28,34 @@ public class RedisService {
             return "Key or value cannot be null.";
         }
 
-        jedis.set(key, value);
-        res.status(201); // Created
-        return "Key-value pair created successfully.";
+        try {
+            jedis.set(key, value);
+            res.status(201); // Created
+            logger.info("Key-value pair created successfully: " + key);
+            return "Key-value pair created successfully.";
+        } catch (Exception e) {
+            logger.error("Error creating key-value pair: " + e.getMessage());
+            res.status(500); // Internal Server Error
+            return "Error creating key-value pair.";
+        }
     }
+
     public String read(Request req, Response res) {
         String key = req.params(":key");
-        String value = jedis.get(key);
 
-        if (value == null) {
-            res.status(404); // Not Found
-            return "No value found for key: " + key;
-        } else {
-            return value;
+        try {
+            String value = jedis.get(key);
+
+            if (value == null) {
+                res.status(404); // Not Found
+                return "No value found for key: " + key;
+            } else {
+                return value;
+            }
+        } catch (Exception e) {
+            logger.error("Error reading key-value pair: " + e.getMessage());
+            res.status(500); // Internal Server Error
+            return "Error reading key-value pair.";
         }
     }
 
@@ -50,19 +68,34 @@ public class RedisService {
             return "Key or value cannot be null.";
         }
 
-        jedis.set(key, value);
-        return "Key-value pair updated successfully.";
+        try {
+            jedis.set(key, value);
+            logger.info("Key-value pair updated successfully: " + key);
+            return "Key-value pair updated successfully.";
+        } catch (Exception e) {
+            logger.error("Error updating key-value pair: " + e.getMessage());
+            res.status(500); // Internal Server Error
+            return "Error updating key-value pair.";
+        }
     }
 
     public String delete(Request req, Response res) {
         String key = req.params(":key");
-        Long result = jedis.del(key);
 
-        if (result == 1) {
-            return "Key deleted successfully.";
-        } else {
-            res.status(404); // Not Found
-            return "No value found for key: " + key;
+        try {
+            Long result = jedis.del(key);
+
+            if (result == 1) {
+                logger.info("Key deleted successfully: " + key);
+                return "Key deleted successfully.";
+            } else {
+                res.status(404); // Not Found
+                return "No value found for key: " + key;
+            }
+        } catch (Exception e) {
+            logger.error("Error deleting key-value pair: " + e.getMessage());
+            res.status(500); // Internal Server Error
+            return "Error deleting key-value pair.";
         }
     }
 }
